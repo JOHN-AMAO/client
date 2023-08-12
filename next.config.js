d196a21e7bf96da783b87e52697c46a43dfcaaac
JOHN-AMAO <amaojohn767@gmail.com>
2023-08-12 13:03:39 +0100

    "last commit"

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "headshots-inc.com",
+        port: "",
+        pathname: "/wp-content/uploads/2021/04/Website-Photo-3.png",
+      },
+    ],
+  },
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         justify-between
       '
       >
-        <div className='flex'>
+        <div className='flex items-center'>
           <div className='relative h-8 w-8 mr-4'>
             <Image
               fill
@@ -26,14 +26,14 @@ export default function Home() {
               src='/logo.png'
             />
           </div>
-          <h1 className={(cn("font-bold text-white"), font.className)}>
+          <h1 className={(cn("font-bold text-white "), font.className)}>
             Genius
           </h1>
         </div>
         <div className='flex items-center gap-x-2'>
           <Button
             variant='outline'
-            className='rounded-full text-black'
+            className='rounded-full text-black font-bold'
           >
             Get Started
           </Button>
@@ -64,6 +64,56 @@ export default function Home() {
           </div>
           <div className='text-sm md:text-xl font-light text-zinc-400'>
             Create Content using AI 10X faster
+          </div>
+        </div>
+        <div className='flex flex-col justify-between lg:flex-row md:flex-row'>
+          <div className='bg-[#2b0226] m-5 p-5 rounded-2xl'>
+            <p>
+              Experience AI-driven artistry! Effortlessly convert images into
+              music and videos. A must-have for creators, unlocking endless
+              inspiration and imagination
+            </p>
+            <div className='mt-5'>
+              <Image
+                className='w-10 h-10 rounded-full'
+                width={500}
+                height={500}
+                alt='Testimonial image'
+                src='https://headshots-inc.com/wp-content/uploads/2021/04/Website-Photo-3.png'
+              />
+            </div>
+          </div>
+          <div className='bg-[#2b0226] m-5 p-5 rounded-2xl'>
+            <p>
+              Unlock boundless creativity! This AI tool seamlessly transforms
+              images into music and videos, an essential for artists and
+              marketers. Ignite your imagination and watch your ideas flourish!
+            </p>
+            <div className='mt-5'>
+              <Image
+                className='w-10 h-10 rounded-full'
+                width={500}
+                height={500}
+                alt='Testimonial image'
+                src='https://headshots-inc.com/wp-content/uploads/2021/04/Website-Photo-3.png'
+              />
+            </div>
+          </div>
+          <div className='bg-[#2b0226] m-5 p-5 rounded-2xl'>
+            <p>
+              Elevate creativity with AI magic! Convert pixels into captivating
+              music and videos effortlessly. Perfect for innovators and content
+              creators seeking limitless expression.
+            </p>
+            <div className='mt-5'>
+              <Image
+                className='w-10 h-10 rounded-full'
+                width={500}
+                height={500}
+                alt='Testimonial image'
+                src='https://headshots-inc.com/wp-content/uploads/2021/04/Website-Photo-3.png'
+              />
+            </div>
           </div>
         </div>
       </main>
